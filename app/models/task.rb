cf45115e9e5d7f8b645e561406fc623d02da30c5
Aleksey <alekseylks@gmail.com>
2013-01-11 22:56:23 +0200

    css

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ActiveRecord::Base
   attr_accessible :name, :description, :priority, :deadline
 
 	validates :name, :presence=>true, :length => {:maximum=> 50}
-  validates :description, :presence=>true, :length => {:maximum=> 250}
+  validates :description,  :length => {:maximum=> 250}
 
 	belongs_to :progect
 	belongs_to :status
